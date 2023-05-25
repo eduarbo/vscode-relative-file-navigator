0ab6725d17d34c791fa623edbb822e29d008f39e
Eduardo Ruiz Macias <eduarbo@gmail.com>
2023-05-24 20:02:52 -0600

    filter out excluded files/dirs

--- src/dirQuickPick.ts
+++ src/dirQuickPick.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode'
 import * as fs from 'fs'
+import * as micromatch from 'micromatch'
 import { ThemeIcons } from 'vscode-ext-codicons'
 
 import { getDirQuickPickCommonProps, getCurrentDirPathFromHistory } from './dirQuickPickUtils'
@@ -16,9 +17,28 @@ export async function createDirQuickPick(directoryPath: string, globalState: vsc
   const changeDirectory = async (props?: Partial<vscode.QuickPick<FileItem>>) => {
     try {
       const currentDirPath = getCurrentDirPathFromHistory(dirQuickOptions.history)
-      dirQuickOptions.files = await fs.promises.readdir(currentDirPath, { withFileTypes: true })
-      const commonProps = getDirQuickPickCommonProps(dirQuickOptions)
+      const allFiles = await fs.promises.readdir(currentDirPath, { withFileTypes: true })
 
+      // Get the exclude settings from VS Code's configuration
+      const excludeSettings = vscode.workspace.getConfiguration('files').get('exclude') as { [key: string]: boolean }
+
+      // Filter files/directories based on the exclude settings
+      dirQuickOptions.files = allFiles.filter((file) => {
+        const filePath = vscode.Uri.file(`${currentDirPath}/${file.name}`).fsPath
+
+        // If any of the exclude patterns match, we exclude this file/directory
+        let isExcluded = false
+        Object.entries(excludeSettings).forEach(([pattern, shouldExclude]) => {
+          if (shouldExclude && micromatch.isMatch(filePath, pattern)) {
+            isExcluded = true
+          }
+        })
+
+        // If none of the patterns matched, this file/directory is included
+        return !isExcluded
+      })
+
+      const commonProps = getDirQuickPickCommonProps(dirQuickOptions)
       Object.assign(quickPick, commonProps, props)
     } catch (error) {
       await handleError(vscode.l10n.t('Error reading directory'))
